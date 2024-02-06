ea4b0ca545bbb9a0490f8b84ed40922fbeb0580a
Muril0Pacheco <pachecomurilo04@gmail.com>
2024-02-06 13:38:33 -0300

    primeiro commit

---
+++ ConsoleApp1/calculadora_de_descontos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace test;
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("______________________________________________________________________________________________");     
+        Console.WriteLine("SEJA BEM-VINDO(A) A CALCULADORA DE DESCONTOS");  
+        Console.WriteLine("(Utilize vírgula em caso de números decimais)\n" );    
+        Console.Write("Informe o valor que receberá o desconto: ");     
+        decimal valor = Convert.ToDecimal(Console.ReadLine().Replace('.', ','));
+        Console.Write("Digite o número correspondente ao desconto(Somente números, não precisa do sinal de porcentagem): ");
+        decimal desconto = Convert.ToDecimal(Console.ReadLine().Replace('.', ','));
+        decimal valdesconto = (valor*desconto)/100;
+        decimal valfinal = valor-valdesconto;
+        Console.WriteLine("\nO resultado é " + Math.Round(valfinal, 2)+".");
+        Console.WriteLine("______________________________________________________________________________________________"); 
+        Console.ReadLine();
+    }
+}
